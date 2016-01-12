8b5d4774fed0b3a4357d970add2c1fd075ef9c48
chuah <chkch79@gmail.com>
2016-01-12 12:28:53 +0800

    cleaning up repository

--- CommAgent/CA_Engine_Release/20130930/GetEncryptedString.bat
+++
@@ -1,2 +0,0 @@
-java -Xms128M -Xmx128M -Dfile.encoding=UTF-8 -cp .;sqljdbc.jar;R3Comm_New.zip;mail.jar com.newspage.CommAgent.appl.GetEncryptedString %1
-
--- CommAgent/CA_Engine_Release/20130930/InstallService.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call set_env.bat
-
-%commagent_home%\R3CA.exe -install %commagent_serv% %JAVA_HOME%\jre\bin\server\jvm.dll -Djava.class.path=%commagent_home%\R3Comm_New.zip;%commagent_home%\sqljdbc.jar;%commagent_home%\mail.jar;%commagent_home%\msutil.jar;%commagent_home%\mssqlserver.jar;%commagent_home%\msbase.jar -Duser.timezone=Asia/Kuala Lumpur -Dfile.encoding=UTF-8 -Xms128M -Xmx256M -start com.newspage.CommAgent.appl.HostAppDaemon -params "-appdir" "%commagent_home%" "start" -stop com.newspage.CommAgent.appl.HostAppDaemon -params "-appdir" "%commagent_home%" "stop" -out %commagent_home%\workdir\log\stdout.log -err %commagent_home%\workdir\log\stderr.log
-
-
-pause
